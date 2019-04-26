aeb8159467f822ddaf10a47d7c781fb4e01846f3
lioshi <lionel.fenneteau@gmail.com>
2019-04-26 10:10:57 +0200

    fix php 7.1

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" \
-    -e "s/^memory_limit.*/memory_limit = ${PHP_MEMORY_LIMIT}/" /etc/php/7.2/apache2/php.ini
+    -e "s/^memory_limit.*/memory_limit = ${PHP_MEMORY_LIMIT}/" /etc/php/7.1/apache2/php.ini
 
 # install db
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
